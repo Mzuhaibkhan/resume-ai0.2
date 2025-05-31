81cc382cb46ea5c5c4316160e63c69fec494e9c1
Mzuhaibkhan <khanzuhaib966@gmail.com>
2025-05-31 16:36:07 +0530

    made changes to init.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
         r"/analyze": {
             "origins": [
                 "http://localhost:8080",
-                "https://resume-ai-gzed.vercel.app"
+                "https://resume-ai0-2.vercel.app"
             ],
             "methods": ["POST"],
             "allow_headers": ["Content-Type"]
